593cfbd4f8e1a802c6e47bccf47c5569f7456f5c
SooJae <Mynameisleesujae@gmail.com>
2019-05-21 23:29:26 +0900

    study Algorithm

--- algorithm/src/algorithm/sortPractice6/InsertionSort6.java
+++ algorithm/src/algorithm/sortPractice6/InsertionSort6.java
@@ -2,4 +2,16 @@ package algorithm.sortPractice6;
 
 public class InsertionSort6 {
 
+	public void sort(int[] a) {
+		int j;
+		for(int i = 1; i<a.length; i++	) {
+			int temp = a[i];
+			for(j = i-1; j>=0;j--) {
+				if(a[j]<temp) break;
+				a[j+1]=a[j];
+			}
+			a[j+1] = temp;
+		}
+		
+	}
 }
