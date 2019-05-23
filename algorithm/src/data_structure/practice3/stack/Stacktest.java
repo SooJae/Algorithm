f95ecc869a80d8d3742aecf7a7f05dadc419b52d
SooJae <mynameisleesujae@gmail.com>
2019-05-23 15:57:26 +0900

    study Stack

---
+++ algorithm/src/data_structure/practice3/stack/Stacktest.java
@@ -0,0 +1,16 @@
+package data_structure.practice3.stack;
+
+public class Stacktest {
+
+	public static void main(String[] args) {
+		Stack3 stk = new Stack3(3);
+		
+		stk.push(10);
+		stk.push(20);
+		stk.push(30);
+		stk.push(40);
+		System.out.println(stk);
+		stk.dump();
+	}
+	
+}
