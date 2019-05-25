52482e1ea9c77f645a25586ebbd04ddc8ece4930
SooJae <Mynameisleesujae@gmail.com>
2019-05-25 23:19:19 +0900

    study Algorithm

---
+++ algorithm/src/algorithm/sortPractice7/Test.java
@@ -0,0 +1,20 @@
+package algorithm.sortPractice7;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		int[] arr = {9,2,5,8,10,3,4,6,1,7};
+		
+		BubbleSort7 bs = new BubbleSort7();
+		InsertionSort7 is = new InsertionSort7();
+		SelectionSort7 ss = new SelectionSort7();
+//		MergeSort7 ms = new MergeSort7();
+//		QuickSort7 qs = new QuickSort7();
+		//qs.sort(arr,0,arr.length-1);
+		is.sort(arr);
+		for(int x: arr) {
+			System.out.print(x + " ");
+		}
+	}
+}
